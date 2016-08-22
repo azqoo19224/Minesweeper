0ccea34d9264d4de84d47629d9623d763475a493
Adam <a19224@yahoo.com.tw>
2016-08-22 03:50:15 +0000

    增加註解

--- Game.php
+++ Game.php
@@ -3,25 +3,25 @@
 echo "<div style='text-align:center; font-size:36px;'>";
 $n = 0;
 $bomb = array();
-
+//陣列塞0
 for ($i = 0; $i <10; $i++) {
     for ($j = 0; $j <10; $j++) {
       $bomb[$i][$j] = "0";   
     }
 }
-
-
-
-
+//亂數炸彈塞入
 while ( $n < 40 ) {
     $i = rand(0,9);
     $j = rand(0,9);
+    //如果陣列沒有炸彈  則塞入
     if ($bomb[$i][$j] == 0) {
         $bomb[$i][$j] = "M";
         $n++;
     }
     
 }
+
+//搜尋炸彈(M) 周圍+1
 for ($i = 0; $i <10; $i++) {
     for ($j = 0; $j <10; $j++) {
         if ($bomb[$i][$j] == "M") {
@@ -53,6 +53,7 @@ for ($i = 0; $i <10; $i++) {
     }
 }
 
+//印出字串
 for ($i = 0; $i <10; $i++) {
     for ($j = 0; $j <10; $j++) {
         echo $bomb[$i][$j]."       ";
@@ -62,7 +63,7 @@ for ($i = 0; $i <10; $i++) {
 }
 
 echo "</div>";
-
+//印出字串
 for ($i = 0; $i <10; $i++) {
     for ($j = 0; $j <10; $j++) {
         echo $bomb[$i][$j];    
