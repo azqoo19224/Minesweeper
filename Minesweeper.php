746b2dadc683977fc9b6330064933f3cc53ba377
Adam <a19224@yahoo.com.tw>
2016-08-23 09:58:20 +0000

    優化程式

--- Minesweeper.php
+++ Minesweeper.php
@@ -1,104 +1,149 @@
 <?php
-//宣告陣列長度.炸彈大小
-$row = 10;
-$column = 10;
-$n = 40;
-$numN = 0;
-//正則 排除字元
-if (isset($_GET["map"])) {
-    if (!preg_match("/^([0-9MN]+)$/",$_GET["map"])) {
-        echo "不符合,輸入字串有非0-9 or M or N等字樣";
-        exit;
-    } 
+$minesweeper = new Minesweeper();
+$str = $_GET['map'];
+$minesweeper->fund($str);
 
-    $a = explode ("N", $_GET["map"]);
-   
-    for($i = 0; $i < 10; $i++){
-        $arr[$i] = str_split($a[$i]);
+class Minesweeper {
+//宣告陣列長度.炸彈大小
+private $result = "不符合,";
+private $row = 10;
+private $column = 10;
+private $n = 40;
+private $numN = 0;
+private $bomb;
+private $arr;
+
+function fund($str){
+    if (isset($str)) {
+        //正則 排除字元
+        if (!preg_match("/^([0-8MN]+)$/",$str)) {
+            $this->result .= "輸入字串有非0-8 or M or N等字樣/n";
+        } 
+        if(preg_match("/m/",$str)) {
+            $str = str_replace("m","M",$str);
+            $this->result .= "地雷大小寫有錯/n";
+        }
+        $a = explode ("N", $str);
+       
+        for($i = 0; $i < 10; $i++) {
+            $this->arr[$i] = str_split($a[$i]);
+        }
+    
+        //比對陣列
+        for ($i = 0; $i < $this->row; $i++) {
+            for ($j = 0; $j < $this->column; $j++) {
+              $this->bomb[$i][$j] = "0";   
+            }
     }
-    //比對陣列
-    $bomb = array();
-    for ($i = 0; $i < $row; $i++) {
-    for ($j = 0; $j < $column; $j++) {
-      $bomb[$i][$j] = "0";   
+        //計算炸彈數
+        $this->num = 0;
+        $this->setbomb();
+        $bombStr = $this->fundBombStr();
+        $this->strCount($bombStr, $str);
+        $this->verificationN($a);
+        $this->bombCount();
+        $this->verificationError($bombStr, $str);
+        $this->verificationEqual($bombStr, $str);
     }
 }
-    //計算炸彈數
-    $num = 0;
-
-    for ($i = 0; $i < $row; $i++) {
-        for ($j = 0; $j < $column; $j++) {
-            if ($arr[$i][$j] == "M") {
-                $bomb[$i][$j] = "M";
-                $num++;
-                
-                if ($arr[$i - 1][$j - 1] != "M") {
-                    $bomb[$i - 1][$j - 1]++;
-                }
-                if ($arr[$i - 1][$j] != "M") {
-                    $bomb[$i - 1][$j]++;
-                }
-                if ($arr[$i - 1][$j + 1] != "M") {
-                    $bomb[$i - 1][$j + 1]++;
-                }
-                if ($arr[$i][$j - 1] != "M") {
-                    $bomb[$i][$j - 1]++;
-                }
-                if ($arr[$i][$j + 1] != "M") {
-                    $bomb[$i][$j + 1]++;
-                }
-                if ($arr[$i + 1][$j - 1] != "M") {
-                    $bomb[$i + 1][$j - 1]++;
-                }
-                if ($arr[$i + 1][$j] != "M") {
-                    $bomb[$i + 1][$j]++;
-                }
-                if ($arr[$i + 1][$j + 1] != "M") {
-                    $bomb[$i + 1][$j + 1]++;
+    //設置炸彈
+    function setbomb(){
+        for ($i = 0; $i < $this->row; $i++) {
+            for ($j = 0; $j < $this->column; $j++) {
+                if ($this->arr[$i][$j] == "M") {
+                    $this->bomb[$i][$j] = "M";
+                    $this->num++;
+                    
+                    if ($this->arr[$i - 1][$j - 1] != "M") {
+                        $this->bomb[$i - 1][$j - 1]++;
+                    }
+                    if ($this->arr[$i - 1][$j] != "M") {
+                        $this->bomb[$i - 1][$j]++;
+                    }
+                    if ($this->arr[$i - 1][$j + 1] != "M") {
+                        $this->bomb[$i - 1][$j + 1]++;
+                    }
+                    if ($this->arr[$i][$j - 1] != "M") {
+                        $this->bomb[$i][$j - 1]++;
+                    }
+                    if ($this->arr[$i][$j + 1] != "M") {
+                        $this->bomb[$i][$j + 1]++;
+                    }
+                    if ($this->arr[$i + 1][$j - 1] != "M") {
+                        $this->bomb[$i + 1][$j - 1]++;
+                    }
+                    if ($this->arr[$i + 1][$j] != "M") {
+                        $this->bomb[$i + 1][$j]++;
+                    }
+                    if ($this->arr[$i + 1][$j + 1] != "M") {
+                        $this->bomb[$i + 1][$j + 1]++;
+                    }
                 }
             }
         }
-    }
-    for ($i = 0; $i < $row; $i++) {
-        for ($j = 0; $j < $column; $j++) {
-            $bombStr .= $bomb[$i][$j];    
-        }
-
-        if ($i != ($row - 1)) {
-            $bombStr .= "N";
-        }
-
-    }
-    //長度判斷
-    if (count(str_split($bombStr)) != count(str_split($_GET["map"]))) {
-        echo "不符合,應該長度為".count(str_split($bombStr))."//現有長度為".count(str_split($_GET["map"]));
-        exit;
-    }
-    //N個數判斷
-    if (count($a) != $row) {
-        echo "不符合,因為N的數目有錯";
-        exit;
-    }
-    //炸彈數判斷
-    if ($num != 40) {
-        echo "不符合，炸彈數因該為".$n."//該字串炸彈數只有".$num;
-        exit;
-    }
-    //答案驗證
-    if ($bombStr != $_GET["map"]) {
-        $b = str_split($bombStr);
-        $gg = str_split($_GET["map"]);
-   
-        for ($i = 0 ;$i < ($row*$column+$row); $i++ ) {
-           
-                if ($b[$i] != $gg[$i]) {
-                    // $ii = $i + 1;
-                   $p .= "第".($i+1)."個答案'".$gg[$i]."'有錯//  答案應該為'".$b[$i]."'//";
-            }
-        }
-        echo "不符合,因為".$p;
-        exit;
     }
     
-    echo "符合";
+    function fundBombStr() {
+        for ($i = 0; $i < $this->row; $i++) {
+            for ($j = 0; $j < $this->column; $j++) {
+                $bombStr .= $this->bomb[$i][$j];    
+            }
+    
+            if ($i != ($this->row - 1)) {
+                $bombStr .= "N";
+            }
+    
+        }
+        return $bombStr;
+    }
+    //長度驗證
+    function strCount($bombStr, $str){
+        if (count(str_split($bombStr)) != count(str_split($str))) {
+        $this->result .= "應該長度為" . count(str_split($bombStr)) . "//現有長度為" . count(str_split($str)) . "/n";
+       
+    }
+    }
+    //驗證N個數及位置是否正確
+    function verificationN($a) {
+        if (count($a) != $this->row) {
+            $this->result .= "因為N的數目有錯/n";
+    }
+      //判斷N的位置
+        for ($i = 0 ;$i < count($a) ;$i++) {
+            if (count($this->arr[$i]) != 10) { 
+                $this->result .= "因為第".($i+1)."個N位置有誤/n";
+            }
+        
+    }
+        
+    }
+    //驗證地雷數
+    function bombCount() {
+        if ($this->num != 40) {
+            $this->result .= "炸彈數因該為" . $this->n . "//該字串炸彈數只有" . $this->num . "/n";
+        }
+    }
+    //答案驗證
+    function verificationError($bombStr, $str){
+        if ($bombStr != $str) {
+            $b = str_split($bombStr);
+            $gg = str_split($str);
+            
+            for ($i = 0 ;$i < ($this->row*$this->column+$this->row); $i++ ) {
+                    if ($b[$i] != $gg[$i]) {
+                        // $ii = $i + 1;
+                      $this->result .= "第" . ($i + 1) . "個答案'" . $gg[$i] . "'有錯//  答案應該為'" . $b[$i] . "'/n";
+                }
+            }
+        }
+    }
+    //最後驗證
+    function verificationEqual($bombStr, $str) {
+        if (str_split($bombStr) == str_split($str) && $bombStr != $str) {
+        echo "符合";
+        } else {
+            echo $this->result;
+        }
+        
+    }
 }
